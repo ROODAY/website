b9cdf38332e22847c0a0acc8daa0360ed9e5a18c
ROODAY <rudytheninja@gmail.com>
2014-10-18 17:46:02 -0400

    fix slide

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(document).ready(function(){
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 				if (target.length) {
 					$('html,body').animate({
-					scrollTop: target.offset().top - $("#navbar").height();
+					scrollTop: target.offset().top - $("#navbar").height()
 					}, 1000);
 					return false;
 				}
