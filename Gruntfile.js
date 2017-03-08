b23b3fafb5fa992004e79112a8210e2a0a2856d0
Rudhra Raveendran <rudytheninja@gmail.com>
2017-03-08 11:48:32 -0500

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -76,5 +76,5 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('default', ['concurrent']);
 	grunt.registerTask('inlinecss', ['inline']);
-	grunt.registerTask('compile', ['sass', 'concat', 'uglify']);
+	grunt.registerTask('compile', ['sass', 'concat', 'uglify']); 
 }
